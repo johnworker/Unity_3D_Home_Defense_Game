f17585fac0ab605b441560866c1fa007bba376d0
hunte <besscomdark@gmail.com>
2024-07-17 22:39:52 +0800

    調整

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public Transform[] spawnPoints;
+    public float spawnInterval = 2f;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnEnemy", spawnInterval, spawnInterval);
+    }
+
+    void SpawnEnemy()
+    {
+        int spawnIndex = Random.Range(0, spawnPoints.Length);
+        Instantiate(enemyPrefab, spawnPoints[spawnIndex].position, spawnPoints[spawnIndex].rotation);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public int score = 0;
+    public TextMeshProUGUI scoreText;
+    public GameObject gameOverPanel;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            RestartGame();
+        }
+    }
+
+    public void AddScore(int points)
+    {
+        score += points;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverPanel.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    void RestartGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
+
